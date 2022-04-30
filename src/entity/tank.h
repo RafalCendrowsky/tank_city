251998a63fcdf6b54465d36a061de593c776c586
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-04-30 19:58:44 +0200

    added constructor and other missing functions

--- src/entity/tank.h
+++ src/entity/tank.h
@@ -1,12 +1,14 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 #include "entity.h"
 #include "bullet.h"
 
 class Tank: public Entity {
 public:
     enum e_direction {RIGHT = 0, LEFT = 1, TOP = 2, BOTTOM = 3};
-    void update();
+    Tank(sf::Image texture, sf::Vector2<double> position, double speed, int hp = 1);;
     void move(e_direction direction);
+    Bullet& shoot();
     void stop();
 private:
     double speed;
