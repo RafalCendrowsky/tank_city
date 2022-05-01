2c535c541babd0aff7f2e71736869277bd24b978
niwanska <01168885@pw.edu.pl>
2022-05-01 23:34:51 +0200

    add setters and getters to tank header

--- src/entity/tank.h
+++ src/entity/tank.h
@@ -3,13 +3,24 @@
 #include "entity.h"
 #include "bullet.h"
 
-class Tank: public Entity {
+class Tank : public Entity
+{
 public:
-    enum e_direction {RIGHT = 0, LEFT = 1, TOP = 2, BOTTOM = 3};
-    Tank(sf::Image texture, sf::Vector2<double> position, b2World& world, double speed, int hp = 1);;
+    enum e_direction
+    {
+        RIGHT = 0,
+        LEFT = 1,
+        TOP = 2,
+        BOTTOM = 3
+    };
+    Tank(sf::Image texture, sf::Vector2<double> position, b2World &world, double speed = 0, int hp = 1);
     void move(e_direction direction);
-    Bullet& shoot();
-    virtual void stop();
+    Bullet &shoot();
+    double getSpeed() { return speed; };
+    int getHp() { return hp; };
+    void setSpeed(double newSpeed) { speed = newSpeed; };
+    void setHp(int newHp) { hp = newHp; };
+
 private:
     double speed;
     int hp;
