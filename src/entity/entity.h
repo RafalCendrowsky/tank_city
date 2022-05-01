6e9244390fac6fc9fc35d520c99266af2b0596ec
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-05-01 20:43:32 +0200

    improved header file for entity

--- src/entity/entity.h
+++ src/entity/entity.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <box2d/b2_world.h>
 
-class Entity:{
+class Entity {
 public:
     Entity(sf::Image texture, sf::Vector2f position, b2World& world, bool dynamic);
     bool isDestroyed();
