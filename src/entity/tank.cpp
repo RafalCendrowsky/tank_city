846722a0b2cbc5c46f68b2fead57e6d96f4a9bbe
niwanska <01168885@pw.edu.pl>
2022-05-02 11:06:10 +0200

    implement move method in tank class

--- src/entity/tank.cpp
+++ src/entity/tank.cpp
@@ -1,18 +1,35 @@
 #include "tank.h"
-#include <memory>
 #include "entity.h"
 #include "bullet.h"
+#include <box2d/b2_math.h>
 
-Tank::Tank(sf::Image texture, sf::Vector2<double> position, b2World& world, double speed = 0, int hp = 1):
-    Entity(texture, position, world, true), hp(hp), speed(speed) {};
+Tank::Tank(sf::Image texture, sf::Vector2<float> position, b2World &world, double speed, int hp) : Entity(texture, position, world, true), hp(hp), speed(speed){};
 
 void Tank::move(Tank::e_direction direction){
-
+    b2Vec2 velocity(0,0);
+    speed = this->getSpeed();
+    switch ( direction )
+    {
+    case 0:
+    velocity.Set(speed, 0);
+    break;
+    case 1:
+    velocity.Set(-speed, 0);
+    break;
+    case 2:
+    velocity.Set(0, speed);
+    break;
+    case 3:
+    velocity.Set(0, -speed);
+    break;
+    }
+    this->getBody()->setLinearVelocity(&velocity);
 };
 
-Bullet& Tank::shoot(){
+Bullet &Tank::shoot()
+{
     std::unique_ptr<Bullet> bulletPtr;
-    b2Vec2 velocity(); //dodać wektor
-    bulletPtr.reset(new Bullet((this->getSprite()).getPosition(), this->getBody().getWorld(), velocity));
+    b2Vec2 velocity(); // dodać wektor - add vector (translated for Rafał)
+    bulletPtr.reset(new Bullet((this->getSprite()).getPosition(), this->getBody()->getWorld(), velocity));
     return *bulletPtr;
 };
