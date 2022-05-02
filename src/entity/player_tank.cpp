21095f4beb7fdf2ddad80343dc1df08be088ed6f
niwanska <01168885@pw.edu.pl>
2022-05-02 12:19:31 +0200

    implement player tank constructor

---
+++ src/entity/player_tank.cpp
@@ -0,0 +1,11 @@
+#include "tank.h"
+#include "entity.h"
+#include "player_tank.h"
+
+PlayerTank::PlayerTank(sf::Image image, sf::Vector2<float> position, b2World &world, int lives) : Tank::Tank(image, position, world){};
+
+void PlayerTank::stop()
+{
+    b2Vec2 velocity(0, 0);
+    this->getBody()->SetLinearVelocity(velocity);
+};
