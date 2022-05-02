f2c0d72f5be1916506be29f762c8c6727126f25a
niwanska <01168885@pw.edu.pl>
2022-05-02 12:08:53 +0200

    improved shoot method

--- src/entity/tank.cpp
+++ src/entity/tank.cpp
@@ -4,36 +4,56 @@
 
 Tank::Tank(sf::Image image, sf::Vector2<float> position, b2World &world, double speed, int hp) : Entity(image, position, world), hp(hp), speed(speed){};
 
-void Tank::move(Tank::eDirection direction){
-    b2Vec2 velocity(0,0);
+void Tank::move(Tank::eDirection direction)
+{
+    b2Vec2 velocity(0, 0);
     speed = this->getSpeed();
-    switch ( direction )
+    switch (direction)
     {
     case 0:
-    velocity.Set(speed, 0);
-    break;
+        velocity.Set(speed, 0);
+        break;
     case 1:
-    velocity.Set(-speed, 0);
-    break;
+        velocity.Set(-speed, 0);
+        break;
     case 2:
-    velocity.Set(0, speed);
-    break;
+        velocity.Set(0, speed);
+        break;
     case 3:
-    velocity.Set(0, -speed);
-    break;
+        velocity.Set(0, -speed);
+        break;
     }
+    this->setDirection(direction);
     this->getBody()->SetLinearVelocity(velocity);
 };
 
-void Tank::stop(){
-    b2Vec2 velocity(0,0);
+void Tank::stop()
+{
+    b2Vec2 velocity(0, 0);
     this->getBody()->SetLinearVelocity(velocity);
 };
 
 Bullet &Tank::shoot()
 {
+    int direction = getDirection();
+    b2Vec2 velocity(0, 0);
+    double bulletSpeed = 2*this->getSpeed();
+    switch (direction)
+    {
+    case 0:
+        velocity.Set(bulletSpeed, 0);
+        break;
+    case 1:
+        velocity.Set(-bulletSpeed, 0);
+        break;
+    case 2:
+        velocity.Set(0, bulletSpeed);
+        break;
+    case 3:
+        velocity.Set(0, -bulletSpeed);
+        break;
+    }
     std::unique_ptr<Bullet> bulletPtr;
-    b2Vec2 velocity(); // dodać wektor - add vector (translated for Rafał)
     bulletPtr.reset(new Bullet((this->getSprite()).getPosition(), this->getBody()->GetWorld(), velocity));
     return *bulletPtr;
 };
