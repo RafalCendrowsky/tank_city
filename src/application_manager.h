9d1b2aca2b8786cd8a98fe3bc1e09e8946610870
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-05-03 17:49:45 +0200

    added const members for b2World.Step function and tracking for current pressed key

--- src/application_manager.h
+++ src/application_manager.h
@@ -11,8 +11,12 @@ public:
 private:
     b2World world = b2World(b2Vec2 {0.0f, 0.0f});
     sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(800, 600), "Battle City");
-    std::vector<Entity> entities;
+    std::vector<Entity*> entities;
     GameManager gameManager;
+    const float timestep = 1.0/60.0;
+    const int32 velocityIterations = 8;
+    const int32 positionIterations = 3;
+    int currentKey;
     void update();
     void render();
     void handleEvents();
