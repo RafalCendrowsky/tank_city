8766516443ddcaea5f6147f567f9f4dc17894eb0
Filip Szyszko <01168917@pw.edu.pl>
2022-05-04 22:41:41 +0200

    taken away incorrect virtual from bullet.h

--- src/entity/indestructiblewall.h
+++ src/entity/indestructiblewall.h
@@ -5,5 +5,5 @@
 class IndestructibleWall : public Entity {
 public:
     IndestructibleWall(sf::Vector2f position, b2World& world);
-    virtual void destroy() override;
+    void destroy() override;
 };
