5ce5a4539a2f4f4ae3e097acfa5fa63a6aa2dc16
niwanska <01168885@pw.edu.pl>
2022-05-31 18:20:09 +0200

    fix target

--- src/resources/map1.cpp
+++ src/resources/map1.cpp
@@ -37,7 +37,6 @@ void crateMap1(sf::RenderWindow &window, MapManager *mapManager){
     mapManager->createWall(wallImage, sf::Vector2 {x, y+70.f});
     mapManager->createWall(wallImage, sf::Vector2 {x, y+3*70.f});
     mapManager->createWall(wallImage, sf::Vector2 {x, y+4*70.f});
-    mapManager->createWall(wallImage, sf::Vector2 {x, y+5*70.f});
 
     mapManager->createWall(wallImage, sf::Vector2 {x, y-70.f});
     mapManager->createWall(wallImage, sf::Vector2 {x, y-3*70.f});
@@ -81,89 +80,5 @@ void crateMap1(sf::RenderWindow &window, MapManager *mapManager){
     mapManager->createWall(wallImage, sf::Vector2 {x-70, y+5*70.f});
     mapManager->createWall(wallImage, sf::Vector2 {x+70, y+4*70.f});
     mapManager->createWall(wallImage, sf::Vector2 {x-70, y+4*70.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+275.f, y+100.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+275.f, y+170.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+275.f, y+240.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+275.f, y+555.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+275.f, y+625.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+275.f, y+695.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+450.f, y+100.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+450.f, y+170.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+450.f, y+240.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+450.f, y+555.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+450.f, y+625.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+450.f, y+695.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1290.f, y+100.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1290.f, y+170.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1290.f, y+240.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1290.f, y+555.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1290.f, y+625.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1290.f, y+695.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1115.f, y+100.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1115.f, y+170.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1115.f, y+240.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1115.f, y+555.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1115.f, y+625.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1115.f, y+695.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+940.f, y+100.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+940.f, y+170.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+940.f, y+240.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+940.f, y+555.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+940.f, y+625.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+940.f, y+695.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+30.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+100.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+170.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+240.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+310.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+380.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+450.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+520.f, y+400.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+870.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+940.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1010.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1080.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1150.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1220.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1290.f, y+400.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+1360.f, y+400.f});
-
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+520.f, y+625.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+590.f, y+625.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+800.f, y+625.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+870.f, y+625.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+520.f, y+170.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+590.f, y+170.f});
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+800.f, y+170.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+870.f, y+170.f});
-
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+100.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+170.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+240.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+310.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+380.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+450.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+485.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+555.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+625.f});
-
-
-    // mapManager->createWall(wallImage, sf::Vector2 {x+625.f, y+818.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+765.f, y+818.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+625.f, y+748.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+765.f, y+748.f});
-    // mapManager->createWall(wallImage, sf::Vector2 {x+695.f, y+748.f});
 
     }
